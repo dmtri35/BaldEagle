2cfc2b318b9c9e35e218afe972f77408fd5d5d05
Tri Dao <daominhtri0503@gmail.com>
2025-06-21 00:54:25 +0000

    debug

--- train/train.py
+++ train/train.py
@@ -109,18 +109,18 @@ head.eval()
 # -------------------------------- Load data --------------------------------
 
 sharegpt_datapaths = list_local_files(sharegpt_datapaths)
-ultra_chat_datapaths = list_local_files(ultra_chat_datapaths)
+# ultra_chat_datapaths = list_local_files(ultra_chat_datapaths)
 
 combined_data_paths = (
-    sharegpt_datapaths[: int(len(sharegpt_datapaths) * 0.95)] + ultra_chat_datapaths
+    sharegpt_datapaths[: int(len(sharegpt_datapaths) * 0.05)]
 )
 random.Random(42).shuffle(combined_data_paths)
 eval_data_paths = sharegpt_datapaths[int(len(sharegpt_datapaths) * 0.95) :][:100]
 
 eagle_train_dataset = EagleLocalDataset(
-    combined_data_paths, transform=AddUniformNoise(std=0.5), max_len=1024
+    combined_data_paths, transform=AddUniformNoise(std=0.5)
 )
-eagle_test_dataset = EagleLocalDataset(eval_data_paths, max_len=1024)
+eagle_test_dataset = EagleLocalDataset(eval_data_paths)
 
 eagle_collator = DataCollatorWithPadding()
 
